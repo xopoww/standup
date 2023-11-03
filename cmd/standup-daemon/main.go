4f30109ae20ea93bf271ec0daa3ed9122dc2cfb9
Arseny Khoroshilov <xopoww@yandex-team.ru>
2023-11-04 02:39:32 +0300

    add default daemon cfg path

--- cmd/standup-daemon/main.go
+++ cmd/standup-daemon/main.go
@@ -19,7 +19,7 @@ func main() {
 		_ = logger.Sync()
 	}()
 
-	flag.StringVar(&args.cfgPath, "config", "", "path to yaml config file")
+	flag.StringVar(&args.cfgPath, "config", daemon.DefaultConfigPath, "path to yaml config file")
 	flag.Parse()
 	if args.cfgPath == "" {
 		logger.Sugar().Fatal("`config` is required.")
