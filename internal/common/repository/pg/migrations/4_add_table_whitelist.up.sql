b8d6fd87cceac307d871f2006f746039b03f4f50
Arseny Khoroshilov <xopoww@yandex-team.ru>
2023-12-30 23:50:03 +0100

    fix migration

--- internal/common/repository/pg/migrations/4_add_table_whitelist.up.sql
+++ internal/common/repository/pg/migrations/4_add_table_whitelist.up.sql
@@ -3,6 +3,6 @@ CREATE TABLE user_whitelist (
     whitelisted BOOLEAN
 );
 
-INSERT INTO user_whitelist (user_id, whitelisted) SELECT id, whitelisted FROM users;
+INSERT INTO user_whitelist (user_id, whitelisted) SELECT id, whitelisted FROM users WHERE id IS NOT NULL;
 
 ALTER TABLE users DROP COLUMN whitelisted;
